dc75faf0009e6c15b35ac68615a4498e76460455
AmarSuryawanshiGit <suryawanshiamar0000@gmail.com>
2025-09-16 22:35:29 +0530

    Project Created By Amar

---
+++ src/test/java/com/dims/DimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
