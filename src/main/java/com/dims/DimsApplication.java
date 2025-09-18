7da8b6771c0f051812d0a69526905c4b045a5a1d
Nileshvalhe02gmail.com <nileshvalhe02@gmail.com>
2025-09-18 21:36:55 +0530

    DIMS:Nil:CUST-1 Title:first commit

--- src/main/java/com/dims/DimsApplication.java
+++ src/main/java/com/dims/DimsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DimsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DimsApplication.class, args);
+		System.out.println("Start my project");
 	}
 
 }
--- src/main/java/com/dims/controller/DimsController.java
+++ src/main/java/com/dims/controller/DimsController.java
@@ -1,5 +1,10 @@
 package com.dims.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
 public class DimsController {
 
 }
